9f14d49ce3f3e2b76320fd7c59b144ced38d3b0c
nveronical <verito.cba933@gmail.com>
2022-09-11 08:37:30 -0300

    Agregando restricciones de clave al usuario

--- parte1.py
+++ parte1.py
@@ -1,11 +1,19 @@
 dic_user = {
-    'user': 'veronica leaño',
+    'nombre': 'veronica leaño',
     'clave': ' '
 }
 
-print("Ingrese la clave: ")
-clave_user = int(input())
+print("Ingrese la clave:")
+clave_user = input()
 for i in range(len(clave_user)):
     dic_user['clave'] += '*'
 
-print(dic_user)
+print("\n",dic_user['clave'])
+
+if len(clave_user) == 4:
+    print("\n+++++++++++++++++++++++Bienvenido/a", dic_user['nombre'],"+++++++++++++++++++++++\n")
+if len(clave_user) > 4:
+    print("❌ Error: La longitud de clave ingresada supera los 4 digitos!\n")
+if len(clave_user) <  4: 
+    print("❌ Error: La longitud de la clave ingresada es inferior a 4 digitos!\n")
+
