5e206a6877242caa9ec53c2d3da86d42c710d87a
nveronical <verito.cba933@gmail.com>
2022-09-11 11:41:24 -0300

    borrando archivo codigo3.py del repositorio remoto

--- codigo3.py
+++
@@ -1,8 +0,0 @@
-menu = ['Deposito', 'Extracción', 'Transferencia', 'Ver Saldo', 'Salir']
-
-print("\n+++++++++++++++++++++++++++++++++++++++++++++++++")
-print("\t\t MENU DE OPCIONES")
-print("+++++++++++++++++++++++++++++++++++++++++++++++++")
-
-for o in menu:
-    print("|--->", o)
